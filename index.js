239626fb6d06cde5130e0bb923c8260e8959f32b
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2018-11-21 06:25:01 -0800

    still figuring out library exports.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-export {default} from './src/engine';
-export {default} from './src/component';
-export {default} from './src/factory';
-export {default} from './src/gameObject';
-export {default} from './src/components/mover';
-export {default} from './src/components/camera';
-export {default} from './src/components/spriteTiler';
-export {default} from './src/library';
+import Engine from 'engine';
+import Component from 'component';
+import Factory from 'factory';
+import GameObject from 'gameObject';
+import Mover from 'components/mover';
+import Camera from 'components/camera';
+import SpriteTiler from 'components/spriteTiler';
+import Library from 'library';
+
+export {Engine, Component, Factory, GameObject, Mover, Camera, SpriteTiler, Library };
 
 export default Gibbon = {
 
