06bb7389be1abc6dde1332bb6a2f0f926b9e9b5d
lerpinglemur <19180097+lerpinglemur@users.noreply.github.com>
2018-11-21 06:14:44 -0800

    more reference fixes. maybe.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import Engine from 'src/engine';
-import Component from 'src/component';
-import Factory from 'src/factory';
-import GameObject from 'src/gameObject';
-import Mover from 'src/components/mover';
-import Camera from 'src/components/camera';
-import SpriteTiler from 'src/components/spriteTiler';
-import Library from 'src/library';
+import Engine from './src/engine';
+import Component from './src/component';
+import Factory from './src/factory';
+import GameObject from './src/gameObject';
+import Mover from './src/components/mover';
+import Camera from './src/components/camera';
+import SpriteTiler from './src/components/spriteTiler';
+import Library from './src/library';
 
 export default Gibbon = {
 
